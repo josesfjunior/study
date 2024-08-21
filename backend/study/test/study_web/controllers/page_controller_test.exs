62fd7f414b51fae74e4fe296433b2a1470c6bf1c
José Júnior <josejuninho8@gmail.com>
2024-08-21 17:13:50 -0300

    first commit

---
+++ backend/study/test/study_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule StudyWeb.PageControllerTest do
+  use StudyWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
